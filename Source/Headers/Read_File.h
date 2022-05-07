e6852596dedeedf988962534de0f465cd6fc1047
2095377 <2095377@students.wits.ac.za>
2022-05-07 14:06:39 +0200

    Read_File V2

--- Source/Headers/Read_File.h
+++ Source/Headers/Read_File.h
@@ -10,12 +10,22 @@
 class Read_File{
 public:
     std::vector<std::vector<std::string>> read(std::string fname, float init_time, float final_time);
-    void print(std::vector<std::vector<std::string>> content);
+    void print(std::vector<std::vector<std::string>> data);
+    //void del_col(std::vector<std::vector<std::string>> &data, int col);
+    void seperate(std::vector<std::vector<std::string>> data);
+    void print1(std::vector<float> val);
+    std::vector<float> get_x(){return x;};
+    std::vector<float> get_y(){return y;};
+    std::vector<float> get_z(){return z;};
+    void prints();
 private:
     //float time;
     int count = 0;
     std::vector<std::vector<std::string>> data;
 	std::vector<std::string> row;
+    std::vector<float> x;
+    std::vector<float> y;
+    std::vector<float> z;
 	std::string line, word;
 };
 #endif
--- Source/Qmain.cpp
+++ Source/Qmain.cpp
@@ -9,8 +9,14 @@ using namespace std;
 
 int main()
 {
+	vector<float> x,y,z;
     Read_File file;
 	std::vector<std::vector<std::string>> data = file.read("Accelerometer.csv", 0.2, 0.3);
+	file.seperate(data);
+	x = file.get_x();
+	y = file.get_y();
+	z = file.get_z();
+	file.prints();
     file.print(data);
  
 	
